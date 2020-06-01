442d56b959d02bce8f46aa67a8ca05dc417a9724
Kosmostars7403 <3699818@mail.ru>
2020-06-01 14:06:27 +0300

    Add argparse

--- main.py
+++ main.py
@@ -3,19 +3,27 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 import collections
+import argparse
+from pprint import pprint
 
 WINERY_CREATION_YEAR = 1920
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Заполняем витрину магазина ')
+    parser.add_argument('file_path', help='Путь в файлу с ассортиментом')
+    args = parser.parse_args()
+    file_path = args.file_path
+
     current_year = datetime.date.today().year
     winery_age = current_year - WINERY_CREATION_YEAR
 
-    wines_excel_data_df = pandas.read_excel('wine3.xlsx')
+    wines_excel_data_df = pandas.read_excel(file_path)
     drinks = wines_excel_data_df.to_dict(orient = 'records')
 
     drinks_categorized = collections.defaultdict(list)
     for drink in drinks:
         drinks_categorized[drink['Категория']].append(drink)
+    pprint(drinks_categorized)
 
     env = Environment(
         loader=FileSystemLoader('.'),
